3e2f7018078e3d45da9d69c95039899145cb1d1e
KrzysztofMoszczynski <luxtorpeda0104@gmail.com>
2021-11-03 13:51:45 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <ul id="todolist">
+    <li v-for="(item, index) in todos" :key="item">
+      <button class="btn" @click="handleDelete(index)">
+        <i class="fa fa-trash"></i>
+      </button>
+      {{ item }}
+    </li>
+  </ul>
+  <input type="text" ref="newThing" />
+  <button @click="handleAdd">Add</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      title: 'Todo list',
+      todos: [],
+    };
+  },
+  methods: {
+    handleAdd() {
+      if (this.$refs.newThing.value.length > 0) {
+        console.log(this.$refs.newThing.value);
+        this.todos.push(this.$refs.newThing.value);
+        this.$refs.newThing.value = '';
+        console.log(this.todos[0]);
+      }
+    },
+    handleDelete(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +42,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  padding-left: 40px;
 }
 </style>
